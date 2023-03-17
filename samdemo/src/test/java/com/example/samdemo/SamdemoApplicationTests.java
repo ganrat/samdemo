03fe6323b6354104707d0e444dbea8ec7740ae87
ganeshkumar.r <ganeshkumar.r@globallogic.com>
2023-03-17 17:01:15 +0530

    Push existing project to GitLab

---
+++ samdemo/src/test/java/com/example/samdemo/SamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
